09e9884921f6777e4d28282898337e269be78b37
Marcio <marcioxerfan@gmail.com>
2023-05-15 12:35:07 -0300

    Feat: scripts, rotas de usuario e controller

---
+++ src/router/usuario.router.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const usuarioController = require("../controller/usuario.controller");
+
+router.get("/findById/:id");
+router.get("/findAll");
+
+router.post("/create");
+router.post("/addAddress/:id");
+
+router.put("/update/:id");
+
+router.delete("/remove/:id");
+router.delete("/removeAddress");
+
+module.exports = router;
