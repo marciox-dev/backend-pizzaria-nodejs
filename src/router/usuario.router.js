3d98377d074c6533acf9377613d273c3ca74dce1
Marcio <marcioxerfan@gmail.com>
2023-05-18 08:57:08 -0300

    Docs: comentarios dos arquivos Index e User Routes

--- src/router/usuario.router.js
+++ src/router/usuario.router.js
@@ -2,14 +2,18 @@ const express = require("express");
 const router = express.Router();
 const usuarioController = require("../controller/usuario.controller");
 
+//rotas GET
 router.get("/findById/:id", usuarioController.findUserByIdController);
 router.get("/findAll", usuarioController.findAllUsersController);
 
+//rotas POST    
 router.post("/create", usuarioController.createUserController);
 router.post("/addAddress/:id", usuarioController.addUserAddressController);
 
+//rotas PUT
 router.put("/update/:id", usuarioController.updateUserController);
 
+//rotas DELETE  
 router.delete("/remove/:id", usuarioController.removeUserController);
 router.delete("/removeAddress", usuarioController.removeUserAddressController);
 
