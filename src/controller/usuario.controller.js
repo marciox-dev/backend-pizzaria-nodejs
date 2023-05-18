d732ed2744a322090c4d2fd1209c4eab77d4a13a
Marcio <marcioxerfan@gmail.com>
2023-05-18 08:48:04 -0300

    Feat: user controller added e rotas atualizadas

---
+++ src/controller/usuario.controller.js
@@ -0,0 +1,94 @@
+
+const findUserByIdController = async (req, res) => {
+    try {
+
+
+        
+    } catch (err) {
+        console.log(`erro: ${err.message}`);
+        return res.status(500).send({ message: `Erro inesperado. Tente novamente!` });
+        
+    }
+};
+
+const findAllUsersController = async (req, res) => {
+    try {
+
+
+        
+    } catch (err) {
+        console.log(`erro: ${err.message}`);
+        return res.status(500).send({ message: `Erro inesperado. Tente novamente!` });
+        
+    }
+};
+
+const createUserController = async (req, res) => {
+    try {
+
+
+        
+    } catch (err) {
+        console.log(`erro: ${err.message}`);
+        return res.status(500).send({ message: `Erro inesperado. Tente novamente!` });
+        
+    }
+};
+
+const updateUserController = async (req, res) => {
+    try {
+
+
+        
+    } catch (err) {
+        console.log(`erro: ${err.message}`);
+        return res.status(500).send({ message: `Erro inesperado. Tente novamente!` });
+        
+    }
+};
+
+const removeUserController = async (req, res) => {
+    try {
+
+
+        
+    } catch (err) {
+        console.log(`erro: ${err.message}`);
+        return res.status(500).send({ message: `Erro inesperado. Tente novamente!` });
+        
+    }
+};
+
+const addUserAddressController = async (req, res) => {
+    try {
+
+
+        
+    } catch (err) {
+        console.log(`erro: ${err.message}`);
+        return res.status(500).send({ message: `Erro inesperado. Tente novamente!` });
+        
+    }
+};
+
+const removeUserAddressController = async (req, res) => {
+    try {
+
+
+        
+    } catch (err) {
+        console.log(`erro: ${err.message}`);
+        return res.status(500).send({ message: `Erro inesperado. Tente novamente!` });
+        
+    }
+};
+
+module.exports = {
+    findUserByIdController,
+    findAllUsersController,
+    createUserController,
+    updateUserController,
+    removeUserController,
+    addUserAddressController,
+    removeUserAddressController
+}
--- src/router/usuario.router.js
+++ src/router/usuario.router.js
@@ -2,15 +2,15 @@ const express = require("express");
 const router = express.Router();
 const usuarioController = require("../controller/usuario.controller");
 
-router.get("/findById/:id");
-router.get("/findAll");
+router.get("/findById/:id", usuarioController.findUserByIdController);
+router.get("/findAll", usuarioController.findAllUsersController);
 
-router.post("/create");
-router.post("/addAddress/:id");
+router.post("/create", usuarioController.createUserController);
+router.post("/addAddress/:id", usuarioController.addUserAddressController);
 
-router.put("/update/:id");
+router.put("/update/:id", usuarioController.updateUserController);
 
-router.delete("/remove/:id");
-router.delete("/removeAddress");
+router.delete("/remove/:id", usuarioController.removeUserController);
+router.delete("/removeAddress", usuarioController.removeUserAddressController);
 
 module.exports = router;
