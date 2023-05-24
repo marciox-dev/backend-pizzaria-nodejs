367f06a017e3b77b0638faec3903e19165d52096
Marcio <marcioxerfan@gmail.com>
2023-05-24 08:55:33 -0300

    Fix: posição do arquivo .env na pasta certa

--- src/service/auth.service.js
+++ src/service/auth.service.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 
 const loginService = (email) => Usuario.findOne({ email: email }).select("senha");
 
-const generateToken = (userId) => jwt.sign({ id: userId }, "yfgidyfgidsgfifas", { expiresIn: 986400} );
+const generateToken = (userId) => jwt.sign({ id: userId }, process.env.SECRET, { expiresIn: 986400} );
 
 module.exports = {
     loginService,
