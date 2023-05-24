ca704d506090a083625ac35b69d79b28b309049e
Marcio <marcioxerfan@gmail.com>
2023-05-24 13:58:00 -0300

    Feat: Validacao usuario Produto e categoria

---
+++ src/middleware/validacao.middleware.js
@@ -0,0 +1,75 @@
+const validaUsuario = (req, res, next) => {
+    if (!req.body.nome) {
+
+        return res.status(400).send({ message: `O campo NOME precisa ser preenchido!` });
+    }
+    if (!req.body.email) {
+
+        return res.status(400).send({ message: `O campo EMAIL precisa ser preenchido!` });
+    }
+    if (!req.body.senha) {
+
+        return res.status(400).send({ message: `O campo SENHA precisa ser preenchido!` });
+    }
+    if (!req.body.imagem) {
+
+        return res.status(400).send({ message: `O campo IMAGEM precisa ser preenchido!` });
+    }
+    if (!req.body.admin) {
+
+        return res.status(400).send({ message: `O campo ADMIN precisa ser preenchido!` });
+    }
+
+    return next();
+}
+
+const validaProduto = (req, res, next) => {
+    let erros = []; //var para acumular os erros
+
+    if (!req.body.sabor) {
+        erros.push("sabor");
+    }
+    if (!req.body.tamanho) {
+        erros.push("nome");
+    }
+    if (!req.body.precoUnit) {
+        erros.push("precoUnit");
+    }
+    if (!req.body.imagem) {
+        erros.push("imagem");
+    }
+    if (!req.body.referencia) {
+        erros.push("referencia");
+    }
+
+    //testando quantos erros temos e toma uma decisão
+    if (erros.length == 0) {
+        return next();
+    } else {
+        if (erros.length > 1) {
+            return res.status(400).send({ message: `Os campos ${erros} precisam ser preenchidos!` });
+        } else {
+            return res.status(400).send({ message: `O campo ${erros} precisa ser preenchido!` });
+        }
+    }
+
+
+}
+
+const validaCategoria = (req, res, next) => {
+    if (!req.body.nome) {
+        return res.status(400).send({ message: `O campo NOME precisa ser preenchido!` });
+    }
+}
+
+
+module.exports = {
+    validaUsuario,
+    validaProduto,
+    validaCategoria
+}
+
+//{
+ //   "email": "mes@agosto.com",
+  //  "senha": "agosto"
+   // }
--- src/router/usuario.router.js
+++ src/router/usuario.router.js
@@ -3,20 +3,21 @@ const router = express.Router();
 const usuarioController = require("../controller/usuario.controller");
 
 const authMiddleware = require("../middleware/auth.middleware");
+const { validaUsuario } = require("../middleware/validacao.middleware");
 
 //rotas GET
 router.get("/findById/:id", authMiddleware, usuarioController.findUserByIdController);
 router.get("/findAll", authMiddleware, usuarioController.findAllUsersController);
 
 //rotas POST    
-router.post("/create", usuarioController.createUserController);
-router.post("/addAddress/:id", usuarioController.addUserAddressController);
+router.post("/create", validaUsuario, usuarioController.createUserController);
+router.post("/addAddress/:id", authMiddleware, usuarioController.addUserAddressController);
 
 //rotas PUT
-router.put("/update/:id", usuarioController.updateUserController);
+router.put("/update/:id", authMiddleware, validaUsuario, usuarioController.updateUserController);
 
 //rotas DELETE  
-router.delete("/remove/:id", usuarioController.removeUserController);
-router.delete("/removeAddress", usuarioController.removeUserAddressController);
+router.delete("/remove/:id", authMiddleware, usuarioController.removeUserController);
+router.delete("/removeAddress", authMiddleware, usuarioController.removeUserAddressController);
 
 module.exports = router;
